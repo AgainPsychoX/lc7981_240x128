85be863cff5543d14360a9bd1c4ffa046413a4da
Patryk (PsychoX) Ludwikowski <patryk.ludwikowski.7@gmail.com>
2024-01-03 18:42:17 +0100

    Breakout game example: Fixes for collisions, some testing stuff and more

--- examples/breakout/breakout.ino
+++ examples/breakout/breakout.ino
@@ -22,10 +22,24 @@ constexpr uint8_t displayHeight = 128;
 // #include "examples/testing/fastio_example.hpp"
 // MyDisplay display;
 
-#define BUTTON_LEFT  30
-#define BUTTON_RIGHT 31
+#define BUTTON_A 28
+#define BUTTON_B 29
+#define BUTTON_C 30
+#define BUTTON_D 31
+
+#define BUTTON_LEFT  BUTTON_C
+#define BUTTON_RIGHT BUTTON_D
 
 void(* crash) (void) = 0;
+
+#define DEBUG_BRICK_HITS
+
+////////////////////////////////////////////////////////////////////////////////
+// Utils methods
+
+template <typename T> int sgn(T val) {
+	return (T(0) < val) - (val < T(0));
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // Paddle
@@ -87,6 +101,7 @@ struct BrickHandle
 	uint8_t id;
 
 	inline bool isValid() { return id < bricksCountX * bricksCountY; }
+	inline bool isSolid() { return getType() != BRICK_NONE; }
 
 	inline BrickType getType() { return bricksData[id]; }
 	inline void setType(BrickType type) { bricksData[id] = type; }
@@ -99,16 +114,17 @@ struct BrickHandle
 	}
 
 	/// Updates brick on hit. 
-	/// Returns true if brick was solid (ball should bounce off), false otherwise.
-	inline bool hit() {
-	switch (getType()) {
-			case BRICK_NONE: return false;
-			case BRICK_HP_1: setType(BRICK_NONE); return true;
-			case BRICK_HP_2: setType(BRICK_HP_1); return true;
-			case BRICK_HP_3: setType(BRICK_HP_2); return true; 
-			case BRICK_INDESTRUCTIBLE: return true;
+	void hit() {
+		switch (getType()) {
+			case BRICK_HP_1: setType(BRICK_NONE); return;
+			case BRICK_HP_2: setType(BRICK_HP_1); return;
+			case BRICK_HP_3: setType(BRICK_HP_2); return; 
+			default: return;
 		}
 	}
+
+	inline bool operator==(const BrickHandle& other) const { return id == other.id; }
+	inline bool operator!=(const BrickHandle& other) const { return !(*this == other); }
 };
 
 constexpr BrickHandle const BrickHandle::invalid { static_cast<uint8_t>(-1) };
@@ -172,6 +188,9 @@ void drawHorizontalLineSafely(int16_t x, int16_t y, uint8_t length, uint8_t colo
 		length += x;
 		x = 0;
 	}
+	// if (displayWidth < x) {
+	// 	return;
+	// }
 	if (displayWidth < x + length) {
 		length = displayWidth - x;
 	}
@@ -189,6 +208,9 @@ void drawBall(uint8_t centerX, uint8_t centerY, uint8_t color)
 
 void updateBallAgainstBricks(uint8_t ballCenterX, uint8_t ballCenterY)
 {
+	////////////////////////////////////////
+	// Find corners
+
 	/* Corners:
 		A B - pos(A)=(x1, y1), pos(B)=(x2, y1),
 		C D - pos(C)=(x1, y2), pos(B)=(x2, y2). 
@@ -206,7 +228,7 @@ void updateBallAgainstBricks(uint8_t ballCenterX, uint8_t ballCenterY)
 	}
 
 	constexpr uint8_t topOffset = ballCollisionBoxRadius + bricksGridStartY;
-	bool hasY1 = ballCenterY >= topOffset && ballCenterY < (lastPossibleBrickY - ballCollisionBoxRadius);
+	bool hasY1 = ballCenterY >= topOffset && ballCenterY <= (lastPossibleBrickY + ballCollisionBoxRadius);
 	if (hasY1) {
 		uint8_t sy1 = ballCenterY - topOffset;
 		gy1 = sy1 / (brickHeight + bricksGridGapY);
@@ -234,37 +256,30 @@ void updateBallAgainstBricks(uint8_t ballCenterX, uint8_t ballCenterY)
 	BrickHandle b = hasX2 && hasY1 ? BrickHandle::onGrid(gx2, gy1) : BrickHandle::invalid;
 	BrickHandle c = hasX1 && hasY2 ? BrickHandle::onGrid(gx1, gy2) : BrickHandle::invalid;
 	BrickHandle d = hasX2 && hasY2 ? BrickHandle::onGrid(gx2, gy2) : BrickHandle::invalid;
-	
-	bool aHit = a.isValid() && a.hit();
-	bool bHit = b.isValid() && b.hit();
-	bool cHit = c.isValid() && c.hit();
-	bool dHit = d.isValid() && d.hit();
-	// FIXME: decouple: no double damage if single brick hit with both corners
 
+	bool aHit = a.isValid() && a.isSolid();
+	bool bHit = b.isValid() && b.isSolid();
+	bool cHit = c.isValid() && c.isSolid();
+	bool dHit = d.isValid() && d.isSolid();
+
+#ifdef DEBUG_BRICK_HITS
 	if (aHit || bHit || cHit || dHit) {
-		Serial.print("bCx: "); Serial.println(ballCenterX);
-		Serial.print("bCy: "); Serial.println(ballCenterY);
-		Serial.print("bVx: "); Serial.println(ballVelocityX);
-		Serial.print("bVy: "); Serial.println(ballVelocityY);
-		Serial.print("gx1: "); Serial.println(gx1);
-		Serial.print("gy1: "); Serial.println(gy1);
-		Serial.print("gx2: "); Serial.println(gx2);
-		Serial.print("gy2: "); Serial.println(gy2);
+		Serial.println("--- Brick(s) hit:");
+		Serial.print("bCx: "); Serial.print(ballCenterX); Serial.print("\tbVx: "); Serial.println(ballVelocityX);
+		Serial.print("bCy: "); Serial.print(ballCenterY); Serial.print("\tbVy: "); Serial.println(ballVelocityY);
+		Serial.print("gx1: "); Serial.print(gx1); Serial.print("\tgx2: "); Serial.println(gx2);
+		Serial.print("gy1: "); Serial.print(gy1); Serial.print("\tgy2: "); Serial.println(gy2);
+		Serial.print("bx1: "); Serial.print(bx1); Serial.print("\tbx2: "); Serial.println(bx2);
+		Serial.print("by1: "); Serial.print(by1); Serial.print("\tby2: "); Serial.println(by2);
 	}
 	if (aHit) { Serial.print("A: "); Serial.print(a.getGridX()); Serial.print(' '); Serial.println(a.getGridY()); }
 	if (bHit) { Serial.print("B: "); Serial.print(b.getGridX()); Serial.print(' '); Serial.println(b.getGridY()); } 
 	if (cHit) { Serial.print("C: "); Serial.print(c.getGridX()); Serial.print(' '); Serial.println(c.getGridY()); }
 	if (dHit) { Serial.print("D: "); Serial.print(d.getGridX()); Serial.print(' '); Serial.println(d.getGridY()); }
+#endif
 
-	if (aHit) drawBrick(a);
-	if (bHit) drawBrick(b);
-	if (cHit) drawBrick(c);
-	if (dHit) drawBrick(d);
-
-	// if (aHit || bHit || cHit || dHit) {
-	//   delay(1000);
-	//   crash();
-	// }
+	////////////////////////////////////////
+	// Change ball velocity
 
 	// TODO: special logic for (rare) triple-hit?
 
@@ -276,29 +291,66 @@ void updateBallAgainstBricks(uint8_t ballCenterX, uint8_t ballCenterY)
 		if (aHit) {
 			/**/ if (0 < ballVelocityX) ballVelocityY *= -1;
 			else if (0 < ballVelocityY) ballVelocityX *= -1;
-			else goto corner;
+			else goto corner_negate_and_swap_velocity;
 		}
 		else if (bHit) {
 			/**/ if (ballVelocityX < 0) ballVelocityY *= -1;
 			else if (0 < ballVelocityY) ballVelocityX *= -1;
-			else goto corner;
+			else goto corner_swap_velocity;
 		}
 		else if (cHit) {
 			/**/ if (0 < ballVelocityX) ballVelocityY *= -1;
 			else if (ballVelocityY < 0) ballVelocityX *= -1;
-			else goto corner;
+			else goto corner_swap_velocity;
 		}
 		else if (dHit) {
 			/**/ if (0 < ballVelocityX) ballVelocityY *= -1;
 			else if (0 < ballVelocityY) ballVelocityX *= -1;
-			else goto corner;
+			else goto corner_negate_and_swap_velocity;
 		}
-		return;
+		goto done_updating_velocity;
 
-		corner: // negate and swap velocity in both directions
-		float tmp = ballVelocityX;
-		ballVelocityX = -ballVelocityY;
-		ballVelocityY = -tmp;
+		corner_negate_and_swap_velocity: {
+			float tmp = ballVelocityX;
+			ballVelocityX = -ballVelocityY;
+			ballVelocityY = -tmp;
+			goto done_updating_velocity;
+		}
+
+		corner_swap_velocity: {
+			float tmp = ballVelocityX;
+			ballVelocityX = ballVelocityY;
+			ballVelocityY = tmp;
+			goto done_updating_velocity;
+		}
+	}
+	done_updating_velocity:
+
+	////////////////////////////////////////
+	// Update hit bricks
+
+	if (aHit) {
+		a.hit();
+		drawBrick(a);
+		Serial.println();
+		if (a == b) bHit = false;
+		if (a == c) cHit = false;
+		if (a == d) dHit = false;
+	}
+	if (bHit) {
+		b.hit();
+		drawBrick(b);
+		if (b == c) cHit = false;
+		if (b == d) dHit = false;
+	}
+	if (cHit) {
+		c.hit();
+		drawBrick(c);
+		if (c == d) dHit = false;
+	}
+	if (dHit) {
+		d.hit();
+		drawBrick(d);
 	}
 
 	// TODO: fix corners bounce based on position in the brick
@@ -316,21 +368,25 @@ void updateBall(uint32_t deltaTime)
 	if (needRedraw) {
 		drawBall(previousBallScreenX, previousBallScreenY, 0x00);
 	}
+	
+	constexpr uint8_t rightMostPossibleBallCenterX = displayWidth - ballCollisionBoxRadius;
 	if (ballX < ballCollisionBoxRadius) {
-		ballX *= -1;
+		ballX = -ballX + ballCollisionBoxRadius * 2; // cast not needed here, fits into uint8_t
 		ballVelocityX *= -1;
 	}
-	else if (ballX >= displayWidth - ballCollisionBoxRadius - 1) {
-		ballX = -ballX + static_cast<float>(displayWidth) * 2;
+	else if (ballX >= rightMostPossibleBallCenterX - 1) {
+		ballX = -ballX + static_cast<float>(rightMostPossibleBallCenterX) * 2; // cast required, overflows uint8_t
 		ballVelocityX *= -1;
 	}
+
+	constexpr uint8_t lowestPaddleHitBallCenterX = paddleY - ballCollisionBoxRadius;
 	if (ballY < ballCollisionBoxRadius) {
-		ballY *= -1;
+		ballY = -ballY + ballCollisionBoxRadius * 2; // cast not needed here, fits into uint8_t
 		ballVelocityY *= -1;
 	}
-	else if (ballY >= paddleY - ballCollisionBoxRadius) {
+	else if (ballY >= lowestPaddleHitBallCenterX) {
 		if (paddleX <= ballX && ballX <= paddleX + paddleWidth) {
-			ballY = -ballY + static_cast<float>(paddleY) * 2;
+			ballY = -ballY + static_cast<float>(lowestPaddleHitBallCenterX) * 2; // cast required, overflows uint8_t
 			ballVelocityY *= -1;
 			// TODO: change velocity based on point on paddle
 		}
@@ -343,8 +399,14 @@ void updateBall(uint32_t deltaTime)
 			return;
 		}
 	}
+
 	uint8_t nextBallScreenX = static_cast<uint8_t>(ballX);
 	uint8_t nextBallScreenY = static_cast<uint8_t>(ballY);
+	if (nextBallScreenX >= displayWidth) {
+		Serial.println(ballX);
+		delay(1000);
+		crash();
+	}
 	updateBallAgainstBricks(nextBallScreenX, nextBallScreenY);
 	if (needRedraw) {
 		drawBall(nextBallScreenX, nextBallScreenY, 0xFF);
@@ -360,8 +422,10 @@ void setup()
 	Serial.begin(115200);
 	Serial.println(F("hello!"));
 
-	pinMode(BUTTON_LEFT,  INPUT_PULLUP);
-	pinMode(BUTTON_RIGHT, INPUT_PULLUP);
+	pinMode(BUTTON_A, INPUT_PULLUP);
+	pinMode(BUTTON_B, INPUT_PULLUP);
+	pinMode(BUTTON_C, INPUT_PULLUP);
+	pinMode(BUTTON_D, INPUT_PULLUP);
 
 	display.initGraphicMode();
 
@@ -369,6 +433,7 @@ void setup()
 }
 
 uint32_t lastUpdate; // ms
+bool automaticPaddle = true; // for testing
 
 void reset()
 {
@@ -398,15 +463,30 @@ void loop()
 		}
 		else {
 			paddleXChange = -1;
+			automaticPaddle = false;
 		}
 	}
 	else if (digitalRead(BUTTON_RIGHT) == HIGH) {
 		paddleXChange = 1;
+		automaticPaddle = false;
+	}
+	if (automaticPaddle) {
+		paddleXChange = (paddleX + paddleWidth / 2) - ballX > 0 ? -1 : 1;
 	}
 	updatePaddle(paddleXChange, 0);
 
+	// Speed up button for testing
+	if (digitalRead(BUTTON_A) == LOW) {
+		ballVelocityX *= 1.01;
+		ballVelocityY *= 1.01;
+	}
+	if (digitalRead(BUTTON_B) == LOW) {
+		ballVelocityX /= 1.01;
+		ballVelocityY /= 1.01;
+	}
+
 	uint32_t now = millis();
-	uint32_t deltaTime = now - lastUpdate; // ms
+	uint32_t deltaTime = now - lastUpdate; // ms  
 	lastUpdate = now;
 
 	updateBall(deltaTime);
